61adfdfe89d3d440872f0ab88af105fbc0e1aca5
Sachin Kumar <137265757+sachinkumar6205@users.noreply.github.com>
2024-12-01 17:41:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+// <!-- typed js effect starts -->
+var typed = new Typed(".typing-text", {
+    strings: ["frontend development", "web designing", "web development"],
+    loop: true,
+    typeSpeed: 50,
+    backSpeed: 25,
+    backDelay: 500,
+});
+// <!-- typed js effect ends -->
+
